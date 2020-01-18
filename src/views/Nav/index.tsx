5a33bf336712f4f380f8e35a1884815a96cdb16a
thaleprechaun <oakinbiyiusman@gmail.com>
2020-01-18 13:08:32 +0100

    modify file

--- src/views/Nav/index.tsx
+++ src/views/Nav/index.tsx
@@ -1,30 +1,42 @@
 import React from 'react';
+
 import Img from '../../Components/Image/Img';
+import Avatar from '../../Components/Avatar';
+import './nav.css';
 
 export default function Navbar() {
   const searchIcon = './assets/search.svg';
+  const notificationBell = './assets/notification_bell.svg';
   return (
     <nav className="nav__container">
       <div className="nav__container--content">
         <div className="nav__items--left">
           <div className="nav__logo">
-            Transmonitor
+            TransMonitor
           </div>
           <div className="nav__search">
-            <Img src={searchIcon} alt="searchIcon"/>
-          </div>
-          <div className="nav__search--input">
-            <input type="search" name="search" className="search__field" placeholder="Search..."/>
+            <Img src={searchIcon} alt="searchIcon" className="search--icon"/>
+            <div className="nav__search--input">
+              <input type="search" name="search" className="search__field" placeholder="Search..."/>
+            </div>
           </div>
         </div>
 
         <div className="nav__items--right">
-          <span>1</span>
-          <span>2</span>
-          <span>3</span>
-          <span>4</span>
-          <span>5</span>
-          <span>6</span>
+          <div className="nav__right--content">
+            <ul className="nav--items">
+              <li className="nav__link">Support</li>
+              <li className="nav__link">FAQ</li>
+              <li className="nav__link">
+                <div className="nav__notif">
+                  <Img src={notificationBell} alt="notificationIcon"/>   
+                </div>
+              </li>
+              <div className="nav__avi">
+                <Avatar />
+              </div>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
