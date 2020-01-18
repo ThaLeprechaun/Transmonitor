99ed9b1ecd64e31bdb81161c6a7535ad6de78b77
thaleprechaun <oakinbiyiusman@gmail.com>
2020-01-18 13:39:01 +0100

    Modify file

--- src/views/Nav/index.tsx
+++ src/views/Nav/index.tsx
@@ -9,33 +9,35 @@ export default function Navbar() {
   const notificationBell = './assets/notification_bell.svg';
   return (
     <nav className="nav__container">
-      <div className="nav__container--content">
-        <div className="nav__items--left">
-          <div className="nav__logo">
-            TransMonitor
-          </div>
-          <div className="nav__search">
-            <Img src={searchIcon} alt="searchIcon" className="search--icon"/>
-            <div className="nav__search--input">
-              <input type="search" name="search" className="search__field" placeholder="Search..."/>
+      <div className="container">
+        <div className="nav__container--content">
+          <div className="nav__items--left">
+            <div className="nav__logo">
+              TransMonitor
+            </div>
+            <div className="nav__search">
+              <Img src={searchIcon} alt="searchIcon" className="search--icon"/>
+              <div className="nav__search--input">
+                <input type="search" name="search" className="search__field" placeholder="Search..."/>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="nav__items--right">
-          <div className="nav__right--content">
-            <ul className="nav--items">
-              <li className="nav__link">Support</li>
-              <li className="nav__link">FAQ</li>
-              <li className="nav__link">
-                <div className="nav__notif">
-                  <Img src={notificationBell} alt="notificationIcon"/>   
+          <div className="nav__items--right">
+            <div className="nav__right--content">
+              <ul className="nav--items">
+                <li className="nav__link">Support</li>
+                <li className="nav__link">FAQ</li>
+                <li className="nav__link">
+                  <div className="nav__notif">
+                    <Img src={notificationBell} alt="notificationIcon"/>   
+                  </div>
+                </li>
+                <div className="nav__avi">
+                  <Avatar />
                 </div>
-              </li>
-              <div className="nav__avi">
-                <Avatar />
-              </div>
-            </ul>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
