d981f77dfe8d7041394f2c5234209bb8f8fae175
thaleprechaun <oakinbiyiusman@gmail.com>
2020-01-21 08:14:02 +0100

    update(Mount layout component): Mount Sidenav, and Main layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import './App.css';
 import Navbar from './views/Nav';
+import SideNav from './views/SideNav';
+import Main from './views/Main/Main';
 
-const App: React.FC = () => {
+export default function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-2">
+            <SideNav />
+          </div>
+          <div className="col-lg-10">
+            <Main />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
