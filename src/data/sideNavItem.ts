f6d68e621c13315c32c15a5c6d1c01c88f75ccb0
thaleprechaun <oakinbiyiusman@gmail.com>
2020-01-21 08:02:20 +0100

    feature(sidenav data): Add data to populate side navigation menu component

---
+++ src/data/sideNavItem.ts
@@ -0,0 +1,20 @@
+// Sidenav menu items
+export const items = {
+  topSideNavItems:  [
+    { text: "All Payments", icon: "assets/payments.svg" },
+    { text: "Reconciled Payments", icon: "assets/reconciled.svg" },
+    { text: "Un-Reconciled Payments", icon: "assets/unreconciled.svg" },
+    { text: "Manual Settlement", icon: "assets/radio.svg" }
+  ],
+  bottomSideNavItems: [
+    { text: "All Orders", icon: "assets/all_orders.svg" },
+    { text: "Pending Orders", icon: "assets/pending_orders.svg" },
+    { text: "Reconciled Orders", icon: "assets/reconciled_orders.svg" }
+  ],
+  transactions: [
+    {transHeader: "Daily Transaction Volume", transValue: "2,342"},
+    {transHeader: "Daily Transaction Value", transValue: "₦4,000,000"},
+    {transHeader: "Total Transaction Volume", transValue: "452,000"},
+    {transHeader: "Total Transaction Value", transValue: "₦4,000,000"},
+  ]
+}
